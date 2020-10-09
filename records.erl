5e14946258e56dd3c7dd48b3e4e9a6c8db79881f
Gutierre-Braga <Gutierre_Braga@outlook.com>
2020-10-09 19:22:09 -0300

    Novos exemplos de cdigo em erlang.

---
+++ records.erl
@@ -0,0 +1,18 @@
+-module(records).
+-compile([export_all]).
+
+-record(person, {
+  name,
+  age,
+
+  status = single
+}).
+
+run() ->
+  P1 = #person{name="Joe Doe", age="25"},
+  
+  io:format("Created person ~p ~n", [P1#person.name]),
+
+  io:format("Records fields: ~p~n", [record_info(fields, person)]),
+
+  io:format("Record size: ~p~n", [record_info(size, person)]).
